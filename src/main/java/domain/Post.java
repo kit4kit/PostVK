edfb2ea40d2a17f992d9160132acd6def83d1ef7
Nikita <trofimov1nv@gmail.com>
2020-11-25 20:03:31 +0300

    Initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,86 @@
+package domain;
+
+public class Post {
+    private int id; // id поста
+    private int ownerId; // id владельца стены
+    private int fromId; // id владельца поста
+    private int date; // дата публикации
+    private int countViews; // колличесто просмотров
+    private String text; // текст записи
+
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private Reposts reposts;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getCountViews() {
+        return countViews;
+    }
+
+    public void setCountViews(int countViews) {
+        this.countViews = countViews;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public Reposts getReposts() {
+        return reposts;
+    }
+
+    public void setReposts(Reposts reposts)g {
+        this.reposts = reposts;
+    }
+}
